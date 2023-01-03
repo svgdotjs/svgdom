0135a43fd13802bce9870ddedcb6b61d563300ac
Thomas Weber <thomas.weber@notengrafik.com>
2023-01-03 09:18:24 +0100

    Fix typo: Use "self" should be "this"

--- src/dom/svg/SVGForeignObjectElement.js
+++ src/dom/svg/SVGForeignObjectElement.js
@@ -4,8 +4,8 @@ import { SVGAnimatedLength } from './SVGAnimatedLength.js'
 import { SVGGraphicsElement } from './SVGGraphicsElement.js'
 
 export class SVGForeignObjectElement extends SVGGraphicsElement {
-  x = new SVGAnimatedLength(self, 'x')
-  y = new SVGAnimatedLength(self, 'y')
-  width = new SVGAnimatedLength(self, 'width')
-  height = new SVGAnimatedLength(self, 'height')
+  x = new SVGAnimatedLength(this, 'x')
+  y = new SVGAnimatedLength(this, 'y')
+  width = new SVGAnimatedLength(this, 'width')
+  height = new SVGAnimatedLength(this, 'height')
 }
--- src/dom/svg/SVGImageElement.js
+++ src/dom/svg/SVGImageElement.js
@@ -2,8 +2,8 @@ import { SVGAnimatedLength } from './SVGAnimatedLength.js'
 import { SVGGraphicsElement } from './SVGGraphicsElement.js'
 
 export class SVGImageElement extends SVGGraphicsElement {
-  x = new SVGAnimatedLength(self, 'x')
-  y = new SVGAnimatedLength(self, 'y')
-  width = new SVGAnimatedLength(self, 'width')
-  height = new SVGAnimatedLength(self, 'height')
+  x = new SVGAnimatedLength(this, 'x')
+  y = new SVGAnimatedLength(this, 'y')
+  width = new SVGAnimatedLength(this, 'width')
+  height = new SVGAnimatedLength(this, 'height')
 }
