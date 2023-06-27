b4b3eba031f6e6ac2c1819cb9d10352c4fa64f2b
Duncan Mak <duncanmak@gmail.com>
2023-06-27 14:13:19 -0400

    Use import.meta.url

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -1,9 +1,7 @@
-import { join } from 'path'
-// import { fileURLToPath } from 'url'
-import { default as __dirname } from './dirname.cjs' // eslint-disable-line
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-// use this as soon as import.meta is standardized
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const fontSize = 16
 export const fontFamily = 'sans-serif'
